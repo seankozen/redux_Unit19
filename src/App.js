343f6630cd85344a571d33928aff379806f482e5
Sean Kozen <sgkozen@gmail.com>
2023-02-08 17:59:18 +0900

    Fetch data

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-// import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
-import { sendCartData } from "./store/cart-slice";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
@@ -16,56 +15,20 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
+  useEffect (() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+
   useEffect(() => {
 
     if(isInitial) {
       isInitial = false;
-      return;  
+      return; 
     }
-
-/*     const sendCartData = async () => {
-       dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      ); 
-       const response = await fetch(
-        "https://redux-food-app-9fa4c-default-rtdb.firebaseio.com/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      } 
-
-       dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      ); 
-    };
-
-    if(isInitial) {
-      isInitial = false;
-      return;  
+    if(cart.changed){
+      dispatch(sendCartData(cart));    
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    }); */
-    
-    dispatch(sendCartData(cart));
-
   }, [cart, dispatch]);
 
   return (
@@ -86,3 +49,5 @@ function App() {
 }
 
 export default App;
+
+
